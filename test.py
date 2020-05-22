a8712907dd08799c4f451a51dda7bbe4c1660c7f
Xeev Lee <xeevlee93@gmail.com>
2020-05-21 22:33:48 -0700

    Initial Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+# Used to clear the terminal screen
+import os
+os.system('clear')
+
+
+fName = 'Xeev'
+age = 27
+favPizza = {
+	'john':'Pepperoni',
+	'Max' : 'Chicken Bacon Ranch',
+	'Mary': 'Cheese'
+}
+
+print(favPizza['Max'])
